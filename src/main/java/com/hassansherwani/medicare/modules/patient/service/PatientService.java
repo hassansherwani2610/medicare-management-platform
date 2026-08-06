7ce2c4c3ccf86c54287156f587abeddb74dd5f9a
Hassan Sherwani <hassansherwani7878@gmail.com>
2026-08-07 00:24:54 +0500

    Implement patient modules's request, response dto and service or business logic

---
+++ src/main/java/com/hassansherwani/medicare/modules/patient/service/PatientService.java
@@ -0,0 +1,10 @@
+package com.hassansherwani.medicare.modules.patient.service;
+
+import com.hassansherwani.medicare.modules.patient.dto.request.PatientRegisterRequest;
+import com.hassansherwani.medicare.modules.patient.dto.response.PatientResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface PatientService {
+    PatientResponse registerPatient(PatientRegisterRequest request, MultipartFile profilePicture);
+    PatientResponse getPatientByUserId(Long userId);
+}
