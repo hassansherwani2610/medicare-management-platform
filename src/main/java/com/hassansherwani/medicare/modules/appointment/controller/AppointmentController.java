547aae91af8b53a71db77245717174843f871b68
Hassan Sherwani <hassansherwani7878@gmail.com>
2026-08-10 21:39:16 +0500

    Implement appointment module's controller's skeleton

--- src/main/java/com/hassansherwani/medicare/modules/appointment/controller/AppointmentController.java
+++ src/main/java/com/hassansherwani/medicare/modules/appointment/controller/AppointmentController.java
@@ -1,4 +1,19 @@
 package com.hassansherwani.medicare.modules.appointment.controller;
 
+import com.hassansherwani.medicare.modules.appointment.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/appointments")
 public class AppointmentController {
+    private final AppointmentService appointmentService;
+
+    @Autowired
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+
 }
